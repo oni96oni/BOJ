5b884e353c4a1d87ffde01c0ee94ce065f8bc431
oni <rainbow2774@naver.com>
2022-03-15 15:38:29 +0900

    실수

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,35 +1,16 @@
 import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String num1 = sc.next(); //734 -> 437
-		String num2 = sc.next();
-		int count = num1.length();
-		int count2 = num2.length();
-		char asb1[] = new char[num1.length()];
-		char asb2[] = new char[num2.length()];
+		int result = 0;
 		
-		for(int i=0; i<num1.length(); i++) {
-			asb1[count -1] = num1.charAt(i);
-			count--;
+		for(int i=3; i<35; i+=5) {
+			result = result + i;
 		}
 		
-		for(int i=0; i<num2.length(); i++) {
-			asb2[count2 -1] = num2.charAt(i);
-			count2--;
-		}
-		
-		int content1 = Integer.parseInt(String.valueOf(asb1));
-		int content2 = Integer.parseInt(String.valueOf(asb2));
-		
-		
-		if(content1 > content2) {
-			System.out.println(content1);
-		}
-		else {
-			System.out.println(content2);
-		}
+		System.out.println(result);
 	}
 }
