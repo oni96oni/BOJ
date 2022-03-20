a8759f53257c633e19e4a6820d8e6032c2f4eed7
oni <rainbow2774@naver.com>
2022-03-20 23:46:54 +0900

    1316 Solved

--- BOJ/src/solved/Solved1316.java
+++ BOJ/src/solved/Solved1316.java
@@ -1,8 +1,10 @@
+package solved;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Solved1316 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
