58fd069c1ade6739c996132d2166a2e1c3c6f2fa
oni <rainbow2774@naver.com>
2022-03-21 16:40:55 +0900

    kb 테스트용

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,30 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		int num = 5; 
+		double num1 = 10.;
 		
-		int idx = sc.nextInt();
-		List<String> arr = new ArrayList<String>();
-		for(int i = 0; i < idx; i++) {
-			arr.add(sc.next());
-		}
-		
-		for(int i = 0; i < arr.size(); i++) {
-			char[] ch = arr.get(i).toCharArray();
-			char tmp = 0;
-			for(int j = 0; j < ch.length - 1; j++) {
-				if(ch[j] != ch[j + 1]) {
-					tmp = ch[j];
-				}
-				if(arr.get(i).substring(j+1, ch.length).contains(String.valueOf(tmp))) {
-					idx--;
-					break;
-				}
-			}
-		}
-		System.out.println(idx);
-	}
+		num1 = num;
+	}	
 }
