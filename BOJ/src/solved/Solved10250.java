e5f64b6782e34dc0f64af62077ca421449689c61
oni <rainbow2774@naver.com>
2022-03-28 11:56:25 +0900

    10250 ACM호텔 solved

--- BOJ/src/solved/Solved10250.java
+++ BOJ/src/solved/Solved10250.java
@@ -1,9 +1,11 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
  
-public class Main {
+public class Solved10250 {
  
 	public static void main(String[] args) throws IOException {
  
