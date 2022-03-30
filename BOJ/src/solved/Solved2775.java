ba8cfada2821ef58401bff90620dea8fb7193247
oni <rainbow2774@naver.com>
2022-03-30 23:00:45 +0900

    boj2775 Solved

--- BOJ/src/solved/Solved2775.java
+++ BOJ/src/solved/Solved2775.java
@@ -1,8 +1,10 @@
+package solved;
+
 import java.io.BufferedReader; 
 import java.io.IOException; 
 import java.io.InputStreamReader; 
 
-public class Main {
+public class Solved2775 {
 	public static void main(String[] args) throws IOException { 
 		//BR, SB, Count 변수 선언 및 입력 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
