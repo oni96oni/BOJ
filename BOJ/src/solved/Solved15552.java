7709ed965cd8b606f5ec341af321d971d007b195
oni <rainbow2774@naver.com>
2022-04-05 12:04:03 +0900

    15552 Solved

--- BOJ/src/solved/Solved15552.java
+++ BOJ/src/solved/Solved15552.java
@@ -1,3 +1,5 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -5,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
  
-public class Main {
+public class Solved15552 {
  
 	public static void main(String[] args) throws IOException {
  
