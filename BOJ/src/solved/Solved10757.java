21916beff31c9442a20582a903e92ea4b7be9cd3
oni <rainbow2774@naver.com>
2022-04-07 15:22:18 +0900

    Solved10757

--- BOJ/src/solved/Solved10757.java
+++ BOJ/src/solved/Solved10757.java
@@ -1,10 +1,12 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.math.BigInteger;
  
-public class Main {
+public class Solved10757 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
