aec652c87113060c2af33fd950f20365bbe5de96
oni <rainbow2774@naver.com>
2022-04-13 11:14:59 +0900

    소수문제 에라토스테네스의 체 알고리즘이용

--- BOJ/src/solved/Solved2581.java
+++ BOJ/src/solved/Solved2581.java
@@ -1,8 +1,10 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
  
-public class Main {
+public class Solved2581 {
  
 	public static boolean prime[];
 	
