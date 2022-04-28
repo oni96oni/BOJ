c0255ca0bfa46f3c5d3fce6b66ff1554e2132894
oni <rainbow2774@naver.com>
2022-04-28 15:31:30 +0900

    연습장

--- BOJ/src/FreeNote.java
+++ BOJ/src/FreeNote.java
@@ -1,11 +1,2 @@
 public class FreeNote {
-	public static void main(String args[]){
-		int score;
-		score=10;
-		float score2=1;
-		char a;
-		a='\t'; // \t가 아스키코드상 9이다.
-		System.out.println(score+a+a+score2);
-		System.out.println(a);
-	}
 }
